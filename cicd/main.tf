d165376c16df28671bf0969e14059b554f8bbdb8
Maciej Malek <malek.maciej@gmail.com>
2024-05-02 19:13:01 +0200

    CICD - v2

--- cicd/main.tf
+++ cicd/main.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "trust" {
     effect = "Allow"
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::850480876735:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
     condition {
       test     = "StringEquals"
@@ -43,4 +43,10 @@ resource "aws_iam_role" "github_actions_demo" {
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = aws_iam_role.github_actions_demo.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  client_id_list = [ "sts.amazonaws.com" ]
 }
